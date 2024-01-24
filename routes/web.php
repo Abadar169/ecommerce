83ec69a0ea305f098922c01aecb11decee1de53d
Abhishek Badar <ab15.badar@gmail.com>
2024-01-24 11:05:36 +0530

    Setting up templates

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('home', function(){
+    return view('home');
+});
 
+Route::get('about', function(){
+    return view('about');
+})->name('about');
 
 Route::get('contact', function(){
     return view('contact');
